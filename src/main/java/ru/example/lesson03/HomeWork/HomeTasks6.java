ed6942fd796ebbbc21566fb29165aa0b166cb5ce
linkoffalexey <linkoffalexey@gmail.com>
2023-04-26 20:49:19 +0300

    merge

--- src/main/java/ru/example/lesson03/HomeWork/HomeTasks6.java
+++ src/main/java/ru/example/lesson03/HomeWork/HomeTasks6.java
@@ -7,28 +7,69 @@ public class HomeTasks6 {
         int a1[] = new int[]{0, 5, 100};
         int a2[] = new int[]{-10, 2, 3, 4, 6, 7, 120};
         System.out.println(Arrays.toString(mergeArrays(a1, a2)));
-        System.out.println(Arrays.toString(mergeArrays(a1, a2)));
+        System.out.println(Arrays.toString(mergeArraysR(a2, a1)));
+        System.out.println(Arrays.toString(merge(a1, a2)));
     }
 
+    public static int[] merge(int[] foo, int[] bar) {
 
-    public static int[] mergeArraysR(int[] a1, int[] a2) {
-        int a[] = new int[a1.length + a2.length];
-        int i = 0;
-        if (a2.length > a1.length) {
-            for (int j = 0; j < a1.length; j++) {
-                for (i = 0; i < a2.length; i++) {
-                    if (a2[j] > a1[i]) {
-                        a[i] = a1[i];
-                    } else if (a2[j + 1] < a1[i]) {
-                        a[i] = a2[j + 1];
-                    } else {
-                        a[i] = a2[j];
-                    }
-                }
+        int fooLength = foo.length;
+        int barLength = bar.length;
+
+        int[] merged = new int[fooLength + barLength];
+
+        int fooPosition, barPosition, mergedPosition;
+        fooPosition = barPosition = mergedPosition = 0;
+
+        while (fooPosition < fooLength && barPosition < barLength) {
+            if (foo[fooPosition] < bar[barPosition]) {
+                merged[mergedPosition++] = foo[fooPosition++];
+            } else {
+                merged[mergedPosition++] = bar[barPosition++];
+            }
+        }
+
+        while (fooPosition < fooLength) {
+            merged[mergedPosition++] = foo[fooPosition++];
+        }
+
+        while (barPosition < barLength) {
+            merged[mergedPosition++] = bar[barPosition++];
+        }
+
+        return merged;
+    }
+
+    public static int[] mergeArraysR(int[] sort1, int[] sort2) {
+
+        int mergeSort[] = new int[sort1.length + sort2.length];
+        int lengthSort1 = sort1.length;
+        int lengthSort2 = sort2.length;
+        int indexSort1, indexSort2, indexMerge;
+        indexSort1 = indexSort2 = indexMerge = 0;
+        while (indexSort1 < lengthSort1 && indexSort2 < lengthSort2) {
+            if (sort1[indexSort1] < sort2[indexSort2]) {
+                mergeSort[indexMerge] = sort1[indexSort1];
+                indexMerge++;
+                indexSort1++;
+
+            } else {
+                mergeSort[indexMerge++] = sort2[indexSort2++];
             }
 
+
         }
-        return a;
+        while (indexSort1 < lengthSort1) {
+            mergeSort[indexMerge++] = sort1[indexSort1++];
+
+        }
+        while (indexSort2 < lengthSort2) {
+            mergeSort[indexMerge++] = sort2[indexSort2++];
+
+        }
+
+
+        return mergeSort;
     }
 
 
