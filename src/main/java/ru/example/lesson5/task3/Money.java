b254f92ee576ac36af7f2a2511ab19feb6542a33
linkoffalexey <linkoffalexey@gmail.com>
2023-09-03 20:31:44 +0300

    money

---
+++ src/main/java/ru/example/lesson5/task3/Money.java
@@ -0,0 +1,22 @@
+package ru.example.lesson5.task3;
+
+import java.math.BigDecimal;
+
+public class Money {
+    int ruble;
+    int copeck;
+
+    int money;
+
+    public Money(int ruble, int copeck) {
+        this.ruble = ruble;
+        this.copeck = copeck;
+        this.money = ruble * 100 + copeck;
+
+
+    }
+public Money plus(Money otherMoney){
+
+}
+
+}
